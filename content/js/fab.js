71a7a8fde7ade1f7f557e07845f942f5e075b640
Sherlock <SanderSalamander@gmail.com>
2017-04-26 09:10:42 +0200

    Added comments to fab.js

--- content/js/fab.js
+++ content/js/fab.js
@@ -1,18 +1,22 @@
 $(document).ready(function() {
   $(".fab").click(function() {
+    // REMOVE CLASS .fabInactive IF IT EXISTS, THEN ADD CLASS .fabActive TO .fab
     if ($(".fab").hasClass("fabInactive")) {
         $(".fab").removeClass("fabInactive").addClass("fabActive");
     } else {
         $(".fab").addClass("fabActive");
     }
     $(".fabLayer").addClass("fabLayerActive");
+    // CHANGE THEME-COLOR OF CHROME BAR AFTER 0.4 SECONDS
     setTimeout(function() {
       $("meta[name='theme-color']").attr('content', '#ff5252');
     }, 400);
   });
   $(".fabClose").click(function() {
+    // REMOVE CLASS .fabActive REPLACE WITH CLASS .fabInactive
     $(".fab").removeClass("fabActive").addClass("fabInactive");
     $(".fabLayer").removeClass("fabLayerActive");
+    // CHANGE THEME-COLOR OF CHROME BAR AFTER 0.1 SECONDS
     setTimeout(function() {
       $("meta[name='theme-color']").attr('content', '#009688')
     }, 100);
